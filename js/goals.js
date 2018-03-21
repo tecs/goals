21ac0193613d4b5bd68aa154d6e874d97b98ae86
Alexander Ivanov <ttecss@gmail.com>
2018-03-21 23:56:07 +0200

    Added initial project templating

---
+++ js/goals.js
@@ -0,0 +1,27 @@
+const GOALS = {
+    /**
+     * A caching wrapper around `document.getElementById()`.
+     * @param {String} name
+     * @returns {HTMLElement}
+     */
+    ui: (() => {
+        const cache = {};
+        return name => {
+            if (!(name in cache)) {
+                cache[name] = document.getElementById(name);
+            }
+            return cache[name];
+        };
+    })(),
+
+    /**
+     * Instantiates and returns the specified template.
+     * @param {String} name
+     * @returns {HTMLElement}
+     */
+    template(name)
+    {
+        const template = GOALS.ui('templates').import.querySelector(`#${name}`).content;
+        return document.importNode(template, true).firstElementChild;
+    }
+};
