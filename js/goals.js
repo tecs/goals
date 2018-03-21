838118734a0c610a236fcc9956c6b7fb4b6b7322
Alexander Ivanov <ttecss@gmail.com>
2018-03-21 23:56:28 +0200

    Added basic task creation

--- js/goals.js
+++ js/goals.js
@@ -23,5 +23,33 @@ const GOALS = {
     {
         const template = GOALS.ui('templates').import.querySelector(`#${name}`).content;
         return document.importNode(template, true).firstElementChild;
+    },
+
+    createTaskList()
+    {
+        const taskList = GOALS.template('taskList');
+        const addTask = GOALS.template('addTask');
+        const tasks = taskList.querySelector('.tasks');
+
+        taskList.insertBefore(addTask, tasks);
+
+        addTask.querySelector('button').addEventListener('click', () => {
+            const task = GOALS.template('task');
+            const input = addTask.querySelector('input');
+            const value = input.value.trim();
+
+            if (!value) {
+                return false;
+            }
+
+            task.querySelector('input').value = value;
+            tasks.appendChild(task);
+
+            input.value = '';
+
+        });
+        return taskList;
     }
 };
+
+window.addEventListener('load', () => document.body.appendChild(GOALS.createTaskList()));
